fdbff06e7dfac769990618d59cbd567223566943
Zybera <dzhelb@bk.ru>
2021-04-21 10:51:06 +0300

    Fixed lw3/task2

--- lw3/task2/script.js
+++ lw3/task2/script.js
@@ -1,49 +1,25 @@
 function isTimeRangesIntersect(timeRange1, timeRange2) {
-  if (isValidTimeRange(timeRange1, timeRange2) === true &&
-      timeRange1[0] <= timeRange2[1] && timeRange1[1] >= timeRange2[0]) {
-    return true;
-  }else {
-    return false;
-  }
+  return isValidTimeRange(timeRange1) &&
+    isValidTimeRange(timeRange2) &&
+    timeRange1[0] <= timeRange2[1] &&
+    timeRange1[1] >= timeRange2[0];
 }
-function isValidTimeRange(timeRange1, timeRange2) {
-  if (Array.isArray(timeRange1) && Array.isArray(timeRange2) &&
+function isValidTimeRange(timeRange) {
+  return Array.isArray(timeRange) &&
+    timeRange.length === 2 &&
+    timeRange[0].length === 5 && timeRange[1].length === 5 &&
 
-      timeRange1.length === 2 &&
-      timeRange2.length === 2 &&
+    timeRange[0][0] + timeRange[0][1] >= 0 &&
+    timeRange[0][0] + timeRange[0][1] < 24 &&
+    timeRange[0][2] === ':' &&
+    timeRange[0][3] + timeRange[0][4] >= 0 &&
+    timeRange[0][3] + timeRange[0][4] < 60 &&
 
-      timeRange1[0].length === 5 && timeRange1[1].length === 5 &&
-      timeRange2[0].length === 5 && timeRange2[1].length === 5 &&
-
-
-      timeRange1[0][0] + timeRange1[0][1] >= 0 &&
-      timeRange1[0][0] + timeRange1[0][1] < 24 &&
-      timeRange1[0][2] === ':' &&
-      timeRange1[0][3] + timeRange1[0][4] >= 0 &&
-      timeRange1[0][3] + timeRange1[0][4] < 60 &&
-
-      timeRange1[1][0] + timeRange1[1][1] >= 0 &&
-      timeRange1[1][0] + timeRange1[1][1] < 24 &&
-      timeRange1[1][2] === ':' &&
-      timeRange1[1][3] + timeRange1[1][4] >= 0 &&
-      timeRange1[1][3] + timeRange1[1][4] < 60 &&
-
-
-      timeRange2[0][0] + timeRange2[0][1] >= 0 &&
-      timeRange2[0][0] + timeRange2[0][1] < 24 &&
-      timeRange2[0][2] === ':' &&
-      timeRange2[0][3] + timeRange2[0][4] >= 0 &&
-      timeRange2[0][3] + timeRange2[0][4] < 60 &&
-
-      timeRange2[1][0] + timeRange2[1][1] >= 0 &&
-      timeRange2[1][0] + timeRange2[1][1] < 24 &&
-      timeRange2[1][2] === ':' &&
-      timeRange2[1][3] + timeRange2[1][4] >= 0 &&
-      timeRange2[1][3] + timeRange2[1][4] < 60) {
-    return true;
-  } else {
-    return false;
-  }
+    timeRange[1][0] + timeRange[1][1] >= 0 &&
+    timeRange[1][0] + timeRange[1][1] < 24 &&
+    timeRange[1][2] === ':' &&
+    timeRange[1][3] + timeRange[1][4] >= 0 &&
+    timeRange[1][3] + timeRange[1][4] < 60;
 }
 isTimeRangesIntersect(['08:30', '09:30'], ['10:30', '12:00']); // return false
 isTimeRangesIntersect(['18:30', '19:30'], ['19:00', '21:00']); // return true
