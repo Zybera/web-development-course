c7c07e499a5d330abe7973c7918bc4d6eb7b4125
Zybera <dzhelb@bk.ru>
2021-04-21 10:33:00 +0300

    lw4/task3

--- lw4/task3/forEachRight.js
+++ lw4/task3/forEachRight.js
@@ -1,4 +1,4 @@
-Array.prototype.forEachRight = function(callback) {
+Array.prototype.forEachRight = function (callback) {
   for (var i = this.length - 1; i >= 0; i--) {
     callback(this[i], i, this);
   }
